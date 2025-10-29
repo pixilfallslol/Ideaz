250f79407b72623f2f8e2c7da9b0851d165e5fa4
Pixil <yayitstoday22@gmail.com>
2025-10-29 14:33:42 -0500

    added typing feature

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+let typingMenu = true
+let t = ""
+let noNoWords = ["Fuck","Shit","Damn","Bitch","Nigger","Nigga"]
+
 let windowWidth = 1920
 let windowHeight = 1080
 
@@ -11,4 +15,26 @@ function setup() {
 
 function draw(){
   background(255)
+  
+  fill(0)
+  text(t,windowHeight/2, windowHeight/2)
+  print(t)
+}
+
+function keyTyped() {
+  if (typingMenu) {
+    t += key
+    t = t.replace("Enter", "");
+  }
+}
+
+function keyPressed() {
+  if (keyCode === ENTER) {
+    t = t+"\n"
+  }
+  if (keyCode === BACKSPACE) {
+    if (t.length > 0) {
+      t = t.substring(0, t.length-1);
+    }
+  }
 }
