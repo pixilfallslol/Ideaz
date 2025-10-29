889162e3e9c80a8671f7efff0100120bff99691d
Pixil <yayitstoday22@gmail.com>
2025-10-29 14:07:17 -0500

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+let windowWidth = 1920
+let windowHeight = 1080
+
+function preload() {
+}
+
+function setup() {
+  let CANVAS = createCanvas(windowWidth, windowHeight)
+  CANVAS.position(0,0)
+}
+
+function draw(){
+  background(255)
+}
